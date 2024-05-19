2d4b761e193f468814330901552619023c273c7e
akd420 <ayanakd112@gmail.com>
2024-05-19 18:07:48 +0600

    fixed footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="footer-gradient">
       <div
-        className="text-[#EEE5FFBD] flex items-center justify-between 2xl:mx-60 xl:mx-20 mx-5 h-14 text-sm font-outfit
+        className="text-[#EEE5FFBD] flex items-center justify-between 2xl:flex-row flex-col-reverse 2xl:mx-60 xl:mx-20 mx-5 2xl:py-0 py-6 gap-4 2xl:h-14 h-24 text-sm font-outfit
       "
       >
         <div>© 2023 Copywrite. All rights reserved by QodeMatrix</div>
